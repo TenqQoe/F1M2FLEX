5149a9410a22550ad6ad560d04bd2f4b3c0640b3
Tenq Boersma <33595@ma-web.nl>
2022-01-21 14:00:13 +0100

    half

---
+++ index.js
@@ -0,0 +1,14 @@
+class MijnClass
+{
+    constructor(div)
+    {
+        this.div = div;
+        this. className = document.getElementsByClassName("board")[0];
+        this.array = [];
+        className.style.display = "";
+    }
+    mijnFunctie(a, b)
+    {
+        return a + b;
+    }
+}
